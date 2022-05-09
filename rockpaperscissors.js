d69d46a039a71abfc6a571f0ae07fa33fb34c65b
Justin Samson <Downsider@Justins-MacBook-Pro.local>
2022-05-08 23:17:16 -0400

    Fixed random number generator

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,22 +1,27 @@
 // computes random number 0, 1 or 2 (rock, paper, scissors)
-function computerPlay(randomNum) {
-    randomNum = Math.floor(Math.random() * randomNum);
-    //console.log(randomNum);
-    
-    // how to return string???
+function computerPlay() {
+    const randomNum = Math.floor(Math.random() * max);
+
     if (randomNum == 0){
         return "Rock";
     }
-    else if (randomNum == 1) {
+    else if(randomNum == 1) {
         return "Paper";
     }
     else {
         return "Scissors";
     }
-    
+}
+// selection of a winner
+// return input from user and random pick from computer
+function playRound(playerSelection, computerSelection) {
+    return "You Lose! " + playerSelection + " beats " + computerSelection;
 }
 
+var max = 3;
+const playerSelection = "rock";
+const computerSelection = computerPlay();
 
-
-// print return rock paper scissors
-console.log(computerPlay(3));
+console.log(computerSelection);
+//print return game winner
+console.log(playRound(playerSelection, computerSelection));
